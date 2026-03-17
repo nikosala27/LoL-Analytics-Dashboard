861a81e84597259c584b2b2836e9510283332166
nikodem-s <nikosala27@gmail.com>
2026-03-17 21:41:12 +0100

    Dodanie filtrowania po dacie w histori gier

--- api-data-readers/RiotAccount.py
+++ api-data-readers/RiotAccount.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 from typing import Optional
 from HttpClient import RiotHttpClient
+from datetime import datetime
 
 @dataclass
 class RiotAccount:
@@ -41,18 +42,42 @@ class RiotAccount:
         self.summoner_level = data["summonerLevel"]
         self.profile_icon_id = data["profileIconId"]
 
-    def get_match_history(self, client: RiotHttpClient, count: int = 20) -> list:
-        return client.get(
-                f"/lol/match/v5/matches/by-puuid/{self.puuid}/ids",
-                params={"count": count}
-            )
+    def get_match_history(self, client:RiotHttpClient, start_date: Optional[datetime] = None, end_date: Optional[datetime] = None, max_results: Optional[int] = None) -> list:
+        PAGE_SIZE = 100
+        all_matches = []
+        offset = 0
+
+        base_params = {}
+        if start_date:
+            base_params["startTime"] = int(start_date.timestamp())
+        
+        if end_date:
+            base_params["endTime"] = int(end_date.timestamp())
+
+        while True:
+            if max_results is not None:
+                remaining = max_results - len(all_matches)
+                if remaining <= 0:
+                    break
+                page_size = min(remaining, PAGE_SIZE)
+            else:
+                page_size = PAGE_SIZE  
+            
+            params ={**base_params, "start": offset, "count": page_size}
+            page = client.get(f"/lol/match/v5/matches/by-puuid/{self.puuid}/ids", params=params)
+            
+            all_matches.extend(page)
+            if len(page) < page_size:
+                break
+            offset += page_size
+        return all_matches
+
+
 
 if __name__ == "__main__":
     global_client = RiotHttpClient(region="europe")   # do kont Riot
     regional_client = RiotHttpClient(region="eun1")   # do danych LoL
     account = RiotAccount.from_riot_id(global_client, "Ulenidas", "4661")
 
-    account.get_summoner(regional_client)
-    account.get_match_history(global_client)
-
-    print(account)
+    match_history = account.get_match_history(global_client, start_date=datetime(2026, 3, 1), end_date=datetime(2026, 3, 16))
+    print(match_history)
